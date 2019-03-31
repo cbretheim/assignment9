77541a2a93589dce4d196044704e09356faff13f
cbretheim <33535137+cbretheim@users.noreply.github.com>
2019-03-31 23:08:03 +0200

    Add files via upload

---
+++ assignment9/script.js
@@ -0,0 +1,15 @@
+new Vue ({
+  el: "#myApp",
+  data: {
+    llamaDiet: "Llamas are herbivores",
+    scientificName: "Their scientific name is Lama glama",
+    height: "Llamas are on average 47 inches tall (height at the shoulder)",
+    weightPounds: 250,
+    origin: "Llamas are native to the South American Andes",
+    packs: "Llamas can carry loads of 50-75 pounds up to 20 miles",
+    overloaded: "...but if you overload them, llamas may refuse to move and can throw a tantrum until their load is lightened",
+    babies: "Baby llamas are called crias",
+    gestation: "Their gestaion period is ~350 days",
+    speed: "Llamas can run up to 35 miles per hour"
+  }
+})
